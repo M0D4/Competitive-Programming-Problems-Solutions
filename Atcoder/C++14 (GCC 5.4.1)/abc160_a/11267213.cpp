3e87c25294e9cee43c4fabf72617886ec883c1ff
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:59:10 +0200

    Committing files

---
+++ Atcoder/C++14 (GCC 5.4.1)/abc160_a/11267213.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/numeric>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+#define all(v)   v.begin(), v.end()
+#define endl "\n"
+#define pb push_back
+#define F first
+#define S second
+#define THINK_TWICE_CODE_ONCE cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define to_str to_string
+#define isOdd(n)    (n&1)
+#define LLMAX LLONG_MAX
+#define LLMIN LLONG_MIN
+#define IMN INT_MIN
+#define IMX INT_MAX
+#define pow(n, m) power((ll)n, (ll)m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n)+1) : 1)
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef double dd;
+typedef string str;
+typedef vector<int> vecint;
+typedef vector<ll> vecll;
+typedef pair<int, int> pii;
+
+int main()
+{
+    THINK_TWICE_CODE_ONCE
+    //freopen("test cases\\.txt", "r", stdin) ;
+    str s; cin>>s;
+    if(s[2]==s[3] && s[4]==s[5])cout<<"Yes";
+    else cout<<"No";
+    return 0;
+}
+
+
+
+
+
