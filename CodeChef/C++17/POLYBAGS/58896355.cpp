d19a82137319cba65ff4ccc92d6da3de037ed2dc
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 23:00:15 +0200

    Committing files

---
+++ CodeChef/C++17/POLYBAGS/58896355.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+
+#pragma GCC optimize("Ofast")
+
+using namespace std;
+
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    int tc; cin >> tc;
+    while(tc--){
+        int n; cin >> n;
+        cout << (n + 9) / 10 << endl;
+    }
+    return 0;
+}
