97f92b8684e0d5ec2fe09f47eb089883441ff633
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:56:46 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc186_a/18923979.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+
+#pragma GCC optimize("Ofast")
+
+
+using namespace std;
+
+#define sc              scanf
+#define endl            "\n"
+#define sz(x)           (int) (x.size())
+#define all(v)          v.begin(), v.end()
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    
+    int a, b; cin >> a >> b;
+    cout << a/b;
+    return 0;
+}
