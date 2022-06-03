9c498e545aa0e85f274832935f533516d318c403
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 23:00:16 +0200

    Committing files

---
+++ CodeChef/C++17/BLACKJACK/58894888.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+
+#pragma GCC optimize("Ofast")
+
+using namespace std;
+
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    int tc; cin >> tc;
+    while(tc--){
+        int a, b; cin >> a >> b;
+        int diff = 21 - a - b;
+        cout << (diff > 0 && diff <= 10? diff : -1) << endl;
+    }
+    return 0;
+}
