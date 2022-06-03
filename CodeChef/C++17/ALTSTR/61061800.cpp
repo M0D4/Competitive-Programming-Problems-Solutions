199bf91feb0e77eb8f20ca70968ab7435dc6e26d
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:59:47 +0200

    Committing files

---
+++ CodeChef/C++17/ALTSTR/61061800.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+
+#pragma GCC optimize("Ofast")
+
+using namespace std;
+
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    int tc; cin >> tc;
+    while(tc--){
+        int n; cin >> n;
+        string s; cin >> s;
+        int ones = count(all(s), '1'), zeros = n - ones;
+        int mn = min(ones, zeros);
+        cout << 2 * mn + (ones > mn || zeros > mn) << endl;
+    }
+    return 0;
+}
