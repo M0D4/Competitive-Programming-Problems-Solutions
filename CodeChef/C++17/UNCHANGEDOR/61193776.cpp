1263a66e275c3446ef77384adb812970d5313aee
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 23:00:26 +0200

    Committing files

---
+++ CodeChef/C++17/UNCHANGEDOR/61193776.cpp
@@ -0,0 +1,29 @@
+#include "bits/stdc++.h"
+
+#pragma GCC optimize("Ofast")
+
+using namespace std;
+
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    int tc; cin >> tc;
+    while(tc--){
+        int n; cin >> n;
+        int ans = n - 1;
+        while (n >= 2){
+            ans--;
+            n /= 2;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
