e5cf1d9443a076c356e9d756b3618d539080768f
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 23:00:10 +0200

    Committing files

---
+++ CodeChef/C++17/QUALIFY/59409205.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+
+#pragma GCC optimize("Ofast")
+
+using namespace std;
+
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    int tc; cin >> tc;
+    while(tc--){
+        int x, a, b; cin >> x >> a >> b;
+        cout << (a + 2 * b >= x ? "Qualify" : "NotQualify") << endl;
+    }
+    return 0;
+}
