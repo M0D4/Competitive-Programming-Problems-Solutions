9c6234a17ac15ac60981b72ee2f21db2b44b9bc7
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:57:07 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc180_c/17507524.cpp
@@ -0,0 +1,42 @@
+#include "bits/stdc++.h"
+
+#pragma GCC optimize("trapv")
+#pragma GCC optimize("-Ofast")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-funroll-all-loops,-fpeel-loops,-funswitch-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,sse4.2,popcnt,abm,mmx,avx2,tune=native")
+
+using namespace std;
+
+#define endl            "\n"
+#define sz(x)          (int) (x.size())
+#define sc              scanf
+#define all(v)          v.begin(), v.end()
+#define isOdd(n)       (n&1)
+#define pow(n, m)      (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    ll n; cin >> n;
+    vector<ll> v;
+    for(ll i = 1; i * i <= n; i++){
+        if(n % i == 0){
+            v.push_back(i);
+            if(n / i != i) v.push_back(n / i);
+        }
+    }
+    sort(all(v));
+    for(auto &i: v) cout << i << endl;
+    return 0;
+}
