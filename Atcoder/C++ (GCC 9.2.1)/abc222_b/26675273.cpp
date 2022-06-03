5c1c015f301f0d380ecbf6cfe457e06bf7dfc1d7
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:55:25 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc222_b/26675273.cpp
@@ -0,0 +1,37 @@
+#include "bits/stdc++.h"
+
+
+#pragma GCC optimize("Ofast")
+
+
+using namespace std;
+
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define Unique(x)       x.erase(unique(all(x)), x.end())
+#define clr(x, value)    memset(x, value, sizeof(x))
+
+using ll = long long;
+using pii = pair<int, int>;
+using tii = tuple<int, int, int>;
+
+
+///Intermediate Overflow!!! (1 << i), x * y, 2 * x
+
+
+int main() {
+
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    int n, m; cin >> n >> m;
+    int c = 0;
+    for (int i = 0; i < n; ++i) {
+        int x; cin >> x;
+        c += (x < m);
+    }
+    cout << c;
+    return 0;
+}
+
