cc228fb468a45001b0705ac09e3536c5642dfa12
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:58:27 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc166_d/12788321.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <ext/numeric>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+#define all(v)   v.begin(), v.end()
+#define endl "\n"
+#define pb push_back
+#define F first
+#define S second
+#define START cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define isOdd(n)    (n&1)
+#define LLMAX LLONG_MAX
+#define LLMIN LLONG_MIN
+#define IMN INT_MIN
+#define IMX INT_MAX
+#define pow(n, m) power((ll)n, (ll)m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define mem(x, val)    memset(x, val, sizeof(x)) //be careful
+#define sz(x)          (int) x.size()
+#define MOD 1000000007
+#define EPS 1e-7
+#define lol    {cout<<i<<" "<<-j<<endl; goto here;}
+#define lol2    {cout<<i<<" "<<j<<endl; goto here;}
+
+typedef long long ll;
+typedef long double ld;
+typedef double dd;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+
+int main()
+{
+    START
+    //freopen("test cases\\in.txt", "r", stdin) ;
+        ll x; cin>>x;
+        for(ll i = 0; i<=1000; i++){
+            for(ll j = 0; j<=1000; j++){
+                if((ll)power(i, 5) + power(j, 5)==x)lol
+                if((ll)power(i, 5) - power(j, 5)==x)lol2
+            }
+        }
+
+    here:{};
+	return 0;
+}
