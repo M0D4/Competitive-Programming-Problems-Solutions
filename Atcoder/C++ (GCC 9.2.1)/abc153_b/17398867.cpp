fb777a78ebfac2e723f8833edd4ad1931526b4fc
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:57:10 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc153_b/17398867.cpp
@@ -0,0 +1,40 @@
+#include "bits/stdc++.h"
+
+#pragma GCC optimize("trapv")
+#pragma GCC optimize("-Ofast")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-funroll-all-loops,-fpeel-loops,-funswitch-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,sse4.2,popcnt,abm,mmx,avx2,tune=native")
+
+using namespace std;
+
+#define endl            "\n"
+#define sz(x)          (int) (x.size())
+#define sc              scanf
+#define all(v)          v.begin(), v.end()
+#define isOdd(n)       (n&1)
+#define pow(n, m)      (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int n, h; cin >> h >> n;
+    while(n--){
+        int x; cin >> x;
+        h -= x;
+        if(h <= 0) return cout << "Yes", 0;
+    }
+    cout << "No";
+    return 0;
+}
