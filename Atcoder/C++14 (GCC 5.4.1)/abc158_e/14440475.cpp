fef5cd0f8c625e870be5342c625e174edb08b5b8
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:58:03 +0200

    Committing files

---
+++ Atcoder/C++14 (GCC 5.4.1)/abc158_e/14440475.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v)   v.begin(), v.end()
+#define sz(x)    (int) (x.size())
+#define Unique(x) x.erase(unique(all(x)), x.end())
+#define endl "\n"
+#define isOdd(n)    (n&1)
+#define pow(n, m) (ll)powl(n, m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define clr(x, val)    memset(x, val, sizeof(x)) //be careful of value and TLE
+#define MOD 1000000007
+#define EPS 1e-7
+
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+
+int main()
+{
+     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    //freopen("test cases\\in.txt", "r", stdin);
+    //freopen("test cases\\in.txt", "w", stdout);
+    int n, m; cin>>n>>m;
+    string s; cin>>s;
+    ll cnt = 0;
+    if(m == 2 || m == 5){
+        for(int i = 0; i<n; i++){
+            if((s[i] - '0') % m == 0) cnt += i + 1;
+        }
+        cout<<cnt;
+        return 0;
+    }
+    int po10 = 1, suff =0;
+    vector<int> mp(m);
+    mp[suff]++;
+
+    for(int i = n-1; ~i; i--){
+        int dig = s[i] - '0';
+        suff = (suff + dig * po10%m)%m;
+        po10 = (po10 * 10)% m;
+        cnt += mp[suff];
+        mp[suff]++;
+    }
+    cout<<cnt;
+    return 0;
+}
