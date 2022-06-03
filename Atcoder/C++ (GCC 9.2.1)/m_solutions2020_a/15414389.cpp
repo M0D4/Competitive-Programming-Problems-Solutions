40e2d65c69d405b2ec7b266dfeadf29ceffa3bc4
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:57:49 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/m_solutions2020_a/15414389.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v)          v.begin(), v.end()
+#define pb              push_back
+#define sz(x)          (int) (x.size())
+#define endl            "\n"
+#define isOdd(n)       (n&1)
+#define pow(n, m)      (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int x; cin >> x;
+    int y = 1800, z = 1;
+    while(x/y == 0) y -= 200, z++;
+    cout << z;
+    return 0;
+}
