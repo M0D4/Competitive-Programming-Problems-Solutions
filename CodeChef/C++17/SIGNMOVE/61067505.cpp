07aaa35ee6bbe5c16d2d41d6f36809368a345e99
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:59:46 +0200

    Committing files

---
+++ CodeChef/C++17/SIGNMOVE/61067505.cpp
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h"
+
+#pragma GCC optimize("Ofast")
+
+using namespace std;
+
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    int tc; cin >> tc;
+    while(tc--){
+        int n; cin >> n;
+        int x = (n + 1) / 2;
+        if(n & 1) cout << -x << endl;
+        else cout << x << endl;
+    }
+    return 0;
+}
