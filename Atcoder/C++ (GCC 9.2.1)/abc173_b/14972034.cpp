c7fac4bbbdec51c2e6415cab06b5ab18d4dd94be
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:57:53 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc173_b/14972034.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v)   v.begin(), v.end()
+#define pb       push_back
+#define sz(x)    (int) (x.size())
+#define endl "\n"
+#define isOdd(n)    (n&1)
+#define pow(n, m) (ll)powl(n, m)
+#define clr(x, val)    memset(x, val, sizeof(x)) //be careful of value and TLE
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int n; cin >> n;
+    unordered_map<string, int> mp;
+    for(int i = 0; i < n; i++){
+        string s; cin >> s;
+        mp[s]++;
+    }
+    cout << "AC x " << mp["AC"] << endl;
+    cout << "WA x " << mp["WA"] << endl;
+    cout << "TLE x " << mp["TLE"] << endl;
+    cout << "RE x " << mp["RE"] << endl;
+    return 0;
+}
