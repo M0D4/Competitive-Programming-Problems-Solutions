a9469ef893169d3c72d4e4c93d6339be0ce7a6f0
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:58:34 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc165_b/12590274.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <ext/numeric>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+#define all(v)   v.begin(), v.end()
+#define endl "\n"
+#define pb push_back
+#define F first
+#define S second
+#define START cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define isOdd(n)    (n&1)
+#define LLMAX LLONG_MAX
+#define LLMIN LLONG_MIN
+#define IMN INT_MIN
+#define IMX INT_MAX
+#define pow(n, m) power((ll)n, (ll)m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define mem(x, val)    memset(x, val, sizeof(x)) //be careful
+#define sz(x)          (int) x.size()
+#define MOD 1000000007
+#define EPS 1e-7
+
+typedef long long ll;
+typedef long double ld;
+typedef double dd;
+typedef pair<int, int> pii;
+
+int main()
+{
+    START
+    //freopen("test cases\\in.txt", "r", stdin) ;
+    ll n; cin>>n;
+    ll x = 100, cnt = 0;
+    while(x<n) x += x/100, cnt++;
+    cout<<cnt;
+	return 0;
+}
