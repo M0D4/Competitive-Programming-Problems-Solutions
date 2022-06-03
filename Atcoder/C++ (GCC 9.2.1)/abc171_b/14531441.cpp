b8a54520aaa0336068200985776acaf3b30345e8
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:58:01 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc171_b/14531441.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v)   v.begin(), v.end()
+#define sz(x)    (int) (x.size())
+#define Unique(x) x.erase(unique(all(x)), x.end())
+#define endl "\n"
+#define isOdd(n)    (n&1)
+#define pow(n, m) (ll)powl(n, m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define clr(x, val)    memset(x, val, sizeof(x)) //be careful of value and TLE
+#define MOD 1000000007
+#define EPS 1e-7
+
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    //freopen("test cases\\in.txt", "r", stdin);
+    //freopen("test cases\\in.txt", "w", stdout);
+
+    int n, k; cin >> n >> k;
+    int a[n];
+    for(int &i: a) cin >> i;
+    sort(a, a+ n);
+    int sum = 0;
+    for(int i = 0; i<k; i++)
+        sum += a[i];
+    cout << sum;
+    return 0;
+}
