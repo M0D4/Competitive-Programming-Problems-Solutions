6e24370299505725bba1e2b5e3c1b5735734b1b0
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:58:43 +0200

    Committing files

---
+++ Atcoder/C++14 (GCC 5.4.1)/abc050_b/12231446.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/numeric>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+#define all(v)   v.begin(), v.end()
+#define endl "\n"
+#define pb push_back
+#define F first
+#define S second
+#define START cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define isOdd(n)    (n&1)
+#define LLMAX LLONG_MAX
+#define LLMIN LLONG_MIN
+#define IMN INT_MIN
+#define IMX INT_MAX
+#define pow(n, m) power((ll)n, (ll)m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define mem(x, val)    memset(x, val, sizeof(x)) //be careful
+#define var auto
+#define MOD 1000000007
+
+
+typedef long long ll;
+typedef long double ld;
+typedef double dd;
+typedef pair<int, int> pii;
+
+
+int main()
+{
+    START
+    //freopen("test cases\\in.txt", "r", stdin) ;
+    int n; cin>>n;
+    ll sum = 0;
+    int arr[n];
+    for(int i = 0; i<n; i++)cin>>arr[i], sum += arr[i];
+    int m; cin>>m;
+    while(m--){
+        int i, s; cin>>i>>s;
+        i--;
+        cout<<sum - arr[i] + s<<endl;
+    }
+	return 0;
+}
