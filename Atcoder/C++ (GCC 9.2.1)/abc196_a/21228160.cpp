257d18065ff9c58a3c93383e1329709d727001a8
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:56:21 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc196_a/21228160.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+
+
+#pragma GCC optimize("Ofast")
+
+
+using namespace std;
+
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define Unique(x)       x.erase(unique(all(x)), x.end())
+#define clr(x, last)    memset(x, last, sizeof(x))
+
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using pii = pair<int, int>;
+using tii = tuple<int, int, int>;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    int a, b, c, d; cin >> a >> b >> c >> d;
+    cout << max(a - d, b - c);
+    return 0;
+}
