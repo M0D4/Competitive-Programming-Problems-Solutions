39968871a386b48d54bf6ba3833d73e66737921f
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:56:03 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/zone2021_a/22292640.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+
+
+#pragma GCC optimize("Ofast")
+
+
+using namespace std;
+
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define clr(x, last)    memset(x, last, sizeof(x))
+
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using pii = pair<int, int>;
+using tii = tuple<int, int, int>;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    string s; cin >> s;
+    int c = 0;
+    for (int i = 0; i + 4 <= 12; ++i) {
+        if(s.substr(i, 4) == "ZONe") c++;
+    }
+    cout << c;
+    return 0;
+}
