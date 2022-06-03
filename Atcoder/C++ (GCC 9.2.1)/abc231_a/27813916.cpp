a1d7a3e3bf4c4f0c2b1faa2f223518186057f9f1
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 22:55:06 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc231_a/27813916.cpp
@@ -0,0 +1,22 @@
+#include "bits/stdc++.h"
+
+#pragma GCC optimize("Ofast")
+
+using namespace std;
+
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    double x; cin >> x;
+    cout << fixed << setprecision(9);
+    cout << x / 100;
+    return 0;
+}
