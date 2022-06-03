b7babe6b853bcccbdbc8b1a868f42d18c67617cb
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-03 23:00:17 +0200

    Committing files

---
+++ CodeChef/C++17/SALE/58892139.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+
+#pragma GCC optimize("Ofast")
+
+using namespace std;
+
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    int tc; cin >> tc;
+    while(tc--){
+        int a, b, c; cin >> a >> b >> c;
+        if (a > b) swap(a, b);
+        if (b > c) swap(b, c);
+        if (a > b) swap(a, b);
+        cout << b + c << endl;
+    }
+    return 0;
+}
