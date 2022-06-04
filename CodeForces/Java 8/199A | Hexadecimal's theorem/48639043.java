dd482b0262f1ebd752325df1ab2d143042e5fd21
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 16:05:26 +0200

    Committing files

---
+++ CodeForces/Java 8/199A | Hexadecimal's theorem/48639043.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+ public class Test1 {
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println("0 0 "+a);
+    } //end main method
+    } //end class
