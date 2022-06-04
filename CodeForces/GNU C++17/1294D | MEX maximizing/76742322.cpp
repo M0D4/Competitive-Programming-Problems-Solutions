ef7b25b17f814f912897c5a31405e871e3255290
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 16:01:44 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1294D | MEX maximizing/76742322.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <ext/numeric>
+ using namespace std;
+using namespace __gnu_cxx;
+ #define all(v)   v.begin(), v.end()
+#define endl "\n"
+#define pb push_back
+#define F first
+#define S second
+#define Th third
+#define START cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define isOdd(n)    (n&1)
+#define LLMAX LLONG_MAX
+#define LLMIN LLONG_MIN
+#define IMN INT_MIN
+#define IMX INT_MAX
+#define pow(n, m) power((ll)n, (ll)m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define mem(x, val)    memset(x, val, sizeof(x)) //be careful
+#define MOD 1000000007
+ typedef long long ll;
+typedef long double ld;
+typedef double dd;
+typedef pair<int, int> pii;
+ const int N = 4e5;
+int freq[N+1];
+int main()
+{
+    START
+    //freopen("test cases\\.txt", "r", stdin) ;
+    int q, x; cin>>q>>x;
+    int MEX = 0;
+    while(q--){
+        int a; cin>>a;
+        freq[a%x]++;
+        while(freq[MEX % x]) freq[MEX % x]--, MEX++;
+        cout<<MEX<<endl;
+    }
+ return 0;
+}
