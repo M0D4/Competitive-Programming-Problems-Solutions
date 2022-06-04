5f6cc68cf917be214e528b9704b75161f3458b62
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:56:00 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1519B | The Cake Is a Lie/114632245.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+  #pragma GCC optimize("Ofast")
+  using namespace std;
+ #define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define clr(x, last)    memset(x, last, sizeof(x))
+ using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using pii = pair<int, int>;
+using tii = tuple<int, int, int>;
+    int main()
+{
+     ios_base::sync_with_stdio(0), cin.tie(0);
+     int t; cin >> t;
+    while (t--){
+        int n, m, k; cin >> n >> m >> k;
+        cout << (n * m - 1 == k ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
