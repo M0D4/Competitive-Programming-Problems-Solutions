85f3c1628e6973df282fbb3b1b7451cc58aaca50
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:58:36 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1420B | Rock and Lever/93678129.cpp
@@ -0,0 +1,41 @@
+#include "bits/stdc++.h"
+ using namespace std;
+ #define endl            "\n"
+#define sz(x)          (int) (x.size())
+#define all(v)          v.begin(), v.end()
+#define isOdd(n)       (n&1)
+#define pow(n, m)      (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+  typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+   int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+     int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        multiset<int> s;
+        for(int i = 0; i < n; i++){
+            int a; cin >> a;
+            s.insert(a);
+        }
+        ll c = 0;
+        for(int bit = 31; ~bit && s.size(); bit--){
+            ll cur = 0;
+            multiset<int> temp = s;
+            for(auto &i: s){
+                if((i & (1 << bit))){
+                    cur++;
+                    temp.erase(temp.find(i));
+                }
+            }
+            c += cur * (cur - 1) / 2;
+            s = temp;
+        }
+        cout << c << endl;
+    }
+    return 0;
+}
