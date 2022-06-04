e72f1a0bedb8c1ab4fbfe65925566b95c90f3fe9
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 16:00:48 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1360D | Buying Shovels/81392657.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/numeric>
+/**
+    HAPPY EID :)
+*/
+using namespace std;
+using namespace __gnu_cxx;
+ #define all(v)   v.begin(), v.end()
+#define sz(x)    (int) x.size()
+#define Unique(x) x.erase(unique(all(x)), x.end())
+#define endl "\n"
+#define pb push_back
+#define F first
+#define S second
+#define START cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define isOdd(n)    (n&1)
+#define IMN INT_MIN
+#define IMX INT_MAX
+#define pow(n, m) power(n, m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define mem(x, val)    memset(x, val, sizeof(x)) //be careful of value and TLE
+#define MOD 1000000007
+ typedef long long ll;
+typedef long double ld;
+typedef double dd;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+  int main()
+{
+    START
+    //freopen("test cases\\in.txt", "r", stdin) ;
+    //freopen("test cases\\out.txt", "w", stdout) ;
+    int t; cin>>t;
+    while(t--){
+        ll n, k; cin>>n>>k;
+        ll mn = n;
+        for(ll i = 1; i*i<=n; i++){
+            if(n%i==0){
+                if(i<=k) mn = min(mn, n/i);
+                if(n/i<=k) mn = min(mn, n/(n/i));
+            }
+        }
+        cout<<mn<<endl;
+    }
+ return 0;
+}
