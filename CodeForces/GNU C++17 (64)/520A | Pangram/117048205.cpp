6e946973242341345ceda22da9d6ffdddbea1684
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:54:37 +0200

    Committing files

---
+++ CodeForces/GNU C++17 (64)/520A | Pangram/117048205.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+ using namespace std;
+ int main()
+{
+   int n,cnt=0 ; cin>>n;
+   string s; cin>>s;
+   int freq[128]={0};
+   for(int i=0; i<n; i++){
+        s[i] = tolower(s[i]);
+        if(!freq[s[i]])cnt++;
+        freq[s[i]]++;
+   }
+   if(cnt==26)cout<<"YES";
+   else cout<<"NO";
+    return 0;
+}
