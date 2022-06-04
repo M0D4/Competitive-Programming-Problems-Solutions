0eaa34b0586b12f7b00b2bc086d99c3fffc31a8b
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 16:00:20 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1365B | Trouble Sort/82878616.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+/**
+       return NO_PAIN ? NO_GAIN : GAIN;
+*/
+using namespace std;
+ #define all(v)   v.begin(), v.end()
+#define sz(x)    (int) x.size()
+#define Unique(x) x.erase(unique(all(x)), x.end())
+#define endl "\n"
+#define pb push_back
+#define F first
+#define S second
+#define START cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define isOdd(n)    (n&1)
+#define pow(n, m) (ll)powl(n, m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define mem(x, val)    memset(x, val, sizeof(x)) //be careful of value and TLE
+#define MOD 1000000007
+#define EPS 1e-7
+ typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+  int main()
+{
+    START
+    //freopen("test cases\\in.txt", "r", stdin) ;
+    //freopen("test cases\\out.txt", "w", stdout);
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        int a[n], b[n], one = 0, zero = 0;
+        for(int &i: a) cin>>i;
+        for(int &i: b) cin>>i, zero += !i, one += i;
+        if(is_sorted(a, a+n) || (zero && one))cout<<"Yes\n";
+        else cout<<"No\n";
+    }
+ return 0;
+}
