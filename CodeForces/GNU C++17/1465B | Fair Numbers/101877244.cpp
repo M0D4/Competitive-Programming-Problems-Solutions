77db5ffdf252f632e01549955bcde878a07b0dda
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:57:03 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1465B | Fair Numbers/101877244.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+ #pragma GCC optimize("Ofast")
+  using namespace std;
+ #define sc              scanf
+#define endl            "\n"
+#define sz(x)           (int) (x.size())
+#define all(v)          v.begin(), v.end()
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+  typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+  bool ok(ll n){
+    ll m = n;
+    while(n){
+        int x = n % 10;
+        n /= 10;
+        if(!x) continue;
+        if(m % x != 0) return 0;
+    }
+    return 1;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+     int t; cin >> t;
+    while(t--){
+        ll n; cin >> n;
+        while(!ok(n)) n++;
+        cout << n << endl;
+    }
+    return 0;
+}
