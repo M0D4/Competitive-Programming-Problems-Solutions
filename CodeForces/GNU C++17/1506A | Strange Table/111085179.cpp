e7df20f1f01b4c2769d5796a63401d0c321d12ef
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:56:21 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1506A | Strange Table/111085179.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+  #pragma GCC optimize("Ofast")
+  using namespace std;
+ #define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define Unique(x)       x.erase(unique(all(x)), x.end())
+#define clr(x, last)    memset(x, last, sizeof(x))
+#define numOfDigits(x)  (x ? (ll)(log10(x)) + 1 : 1)
+ using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using pii = pair<int, int>;
+using tii = tuple<int, int, int>;
+  int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0);
+     int tc; cin >> tc;
+    while (tc--){
+        ll n, m, x; cin >> n >> m >> x;
+        ll c = (x - 1) / n, r = x - c * n - 1;
+        cout << r * m + c + 1<< endl;
+    }
+    return 0;
+}       
