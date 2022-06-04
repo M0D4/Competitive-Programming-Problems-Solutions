eac84016d819bde92961522475a39acacefb9f24
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:55:04 +0200

    Committing files

---
+++ CodeForces/GNU C++17 (64)/344A | Magnets/117047631.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+ using namespace std;
+ int main()
+{
+    int n; cin>>n;
+    int cnt =1;
+    string arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    for(int i=1; i<n; i++){
+        if(arr[i][1]==arr[i-1][0])cnt++;
+    }
+    cout<<cnt;
+    return 0;
+}
