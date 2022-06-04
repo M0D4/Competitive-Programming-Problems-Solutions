d6e375854889ff9f948ebe4d8aed8d94d42099f9
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:57:08 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1462B | Last Year's Substring/101271196.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+ #pragma GCC optimize("Ofast")
+  using namespace std;
+ #define sc              scanf
+#define endl            "\n"
+#define sz(x)           (int) (x.size())
+#define all(v)          v.begin(), v.end()
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+  typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+  int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+     int tt; cin >> tt;
+    while(tt--){
+        int n; cin >> n;
+        string s; cin >> s;
+        bool ok = 0;
+        if(s.substr(0, 4) == "2020") ok = 1;
+        else if(s.substr(0, 3) == "202" && s.back() == '0') ok = 1;
+        else if(s.substr(0, 2) == "20" && s.substr(n - 2, 2) == "20") ok = 1;
+        else if(s[0] == '2' && s.substr(n - 3, 3) == "020") ok = 1;
+        else if(s.substr(n - 4, 4) == "2020") ok = 1;
+         cout << (ok ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
