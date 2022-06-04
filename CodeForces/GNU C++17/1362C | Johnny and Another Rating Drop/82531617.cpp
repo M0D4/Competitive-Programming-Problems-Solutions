9285eab70fca7c20a159a15c6b06adb4e485e174
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 16:00:23 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1362C | Johnny and Another Rating Drop/82531617.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <ext/numeric>
+/**
+       return NO_PAIN ? NO_GAIN : GAIN;
+*/
+using namespace std;
+using namespace __gnu_cxx;
+ #define all(v)   v.begin(), v.end()
+#define sz(x)    (int) x.size()
+#define Unique(x) x.erase(unique(all(x)), x.end())
+#define endl "\n"
+#define pb push_back
+#define F first
+#define S second
+#define START cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define isOdd(n)    (n&1)
+#define pow(n, m) power(n, m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define mem(x, val)    memset(x, val, sizeof(x)) //be careful of value and TLE
+#define MOD 1000000007
+#define EPS 1e-7
+#define MAX (ll) 1e18
+ typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+  int main()
+{
+    START
+    //freopen("test cases\\in.txt", "r", stdin) ;
+    //freopen("test cases\\out.txt", "w", stdout);
+    int t; cin>>t;
+    while(t--){
+        ll n; cin>>n;
+        ll sum = n, p = 2, m = n;
+        while(n/p) sum += n/p, n/=p;
+        cout<<sum<<endl;
+    }
+ return 0;
+}
