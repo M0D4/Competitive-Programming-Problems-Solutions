30a155e5b1809c52f14d1d78122c7e4ecc47ea12
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:57:12 +0200

    Committing files

---
+++ CodeForces/GNU C++17/18C | Stripe/101195990.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+ #pragma GCC optimize("Ofast")
+  using namespace std;
+ #define sc              scanf
+#define endl            "\n"
+#define sz(x)           (int) (x.size())
+#define all(v)          v.begin(), v.end()
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+  typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<ll, ll> pii;
+typedef tuple<int, int, int> tii;
+  int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+      int n; cin >> n;
+    int pfx[n + 2] = {};
+    for(int i = 1; i <= n; i++){
+        int x; cin >> x;
+        pfx[i] += pfx[i - 1] + x;
+    }
+     ll c = 0;
+    for(int i = 1; i < n; i++){
+        if(pfx[i] == pfx[n] - pfx[i])
+            c++;
+    }
+    cout << c;
+    return 0;
+}
