ed17c2971a9fa3bb099f2bd45a7e727f4babce5f
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:55:47 +0200

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1527A | And Then There Were K/116859434.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+  #pragma GCC optimize("-Ofast")
+#pragma GCC optimize("-O2")
+  using namespace std;
+ #define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define Unique(x)       x.erase(unique(all(x)), x.end())
+#define clr(x, val)    memset(x, val, sizeof(x))
+#define numOfDigits(x)  (x ? (ll)(log10(x)) + 1 : 1)
+  using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using pii = pair<int, int>;
+using tii = tuple<int, int, int>;
+ int main()
+{
+     ios_base::sync_with_stdio(0), cin.tie(0);
+     int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        for(int i = 30; ~i; --i){
+            if(1 & (n >> i)){
+                cout << (1LL << i) - 1 << endl;
+                break;
+            }
+        }
+    }
+    return 0;
+}
