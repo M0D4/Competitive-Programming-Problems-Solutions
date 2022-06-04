0bddf826d177d2d6816ee8059ac648f21955e13d
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 16:01:13 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1345A | Puzzle Pieces/79170275.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <ext/numeric>
+/**
+    PROGRESS IS PROGRESS
+    NO MATTER HOW SMALL
+*/
+using namespace std;
+using namespace __gnu_cxx;
+ #define all(v)   v.begin(), v.end()
+#define endl "\n"
+#define pb push_back
+#define F first
+#define S second
+#define START cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define isOdd(n)    (n&1)
+#define LLMAX LLONG_MAX
+#define LLMIN LLONG_MIN
+#define IMN INT_MIN
+#define IMX INT_MAX
+#define pow(n, m) power((ll)n, (ll)m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define mem(x, val)    memset(x, val, sizeof(x)) //be careful
+#define MOD 1000000007
+  typedef long long ll;
+typedef long double ld;
+typedef double dd;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+ int main()
+{
+    START
+    //freopen("test cases\\in.txt", "r", stdin) ;
+    //freopen("test cases\\out.txt", "w", stdout) ;
+    int t; cin>>t;
+    while(t--){
+        ll n, m; cin>>n>>m;
+        if(n>m) swap(n, m);
+        cout<<(n==1 || (n==2 && m==2)?"YES":"NO")<<endl;
+    }
+ return 0;
+}
