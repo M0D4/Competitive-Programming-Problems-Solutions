0cd7d8f176fa4426521ce65e4b81af7efb1aa460
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 21:06:07 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1623C | Balanced Stone Heaps/140950634.cpp
@@ -0,0 +1,37 @@
+#include "bits/stdc++.h"
+ #pragma GCC optimize("Ofast")
+ using namespace std;
+ #define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+ using ll = long long;
+ int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+     int tc; cin >> tc;
+    while(tc--){
+        int n; cin >> n;
+        vector<ll> a(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+        auto b = a;
+         int l = 0, r = 1e9;
+        while (l <= r){
+            int mid = (l + r) >> 1;
+            a = b;
+            for (int i = n - 1; i >= 2; --i) {
+                ll diff = min(a[i] - mid, b[i]);
+                ll x = max(0LL, diff / 3);
+                 a[i] -= 3 * x;
+                a[i - 1] += x;
+                a[i - 2] += 2 * x;
+            }
+             if(*min_element(all(a)) >= mid) l = mid + 1;
+            else r = mid - 1;
+        }
+        cout << l - 1 << endl;
+    }
+    return 0;
+}
