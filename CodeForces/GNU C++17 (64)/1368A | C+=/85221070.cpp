7563c22ffbe93de13d458e9f40b3bd8e936ad473
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:59:37 +0200

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1368A | C+=/85221070.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+ using namespace std;
+ #define all(v)   v.begin(), v.end()
+#define sz(x)    (int) (x.size())
+#define Unique(x) x.erase(unique(all(x)), x.end())
+#define endl "\n"
+#define isOdd(n)    (n&1)
+#define pow(n, m) (ll)powl(n, m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define clr(x, val)    memset(x, val, sizeof(x)) //be careful of value and TLE
+#define MOD 1000000007
+#define EPS 1e-7
+  typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+ ll a, b, n;
+ int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+     int t; cin>>t;
+    while(t--){
+        cin>>a>>b>>n;
+        int c = 0;
+        while(a <= n && b <= n){
+            c++;
+            if(a <= b) a += b;
+            else b += a; 
+        }
+        cout<<c<<endl;
+    }
+    return 0;
+}
