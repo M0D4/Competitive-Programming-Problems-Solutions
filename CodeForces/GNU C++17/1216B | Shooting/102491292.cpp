78115db0ad74c7b14815bce63f68fab93a15f15e
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:57:00 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1216B | Shooting/102491292.cpp
@@ -0,0 +1,29 @@
+#include "bits/stdc++.h"
+ #pragma GCC optimize("Ofast")
+  using namespace std;
+ #define sc              scanf
+#define endl            "\n"
+#define sz(x)           (int) (x.size())
+#define all(v)          v.begin(), v.end()
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+  typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+  int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+     int n; cin >> n;
+    pii a[n];
+    for(auto i = 0; i < n; i++) cin >> a[i].first, a[i].second = i;
+    sort(a, a + n);
+    reverse(a, a + n);
+    ll ans = 0;
+    for(auto i = 0; i < n; i++) ans += i * a[i].first + 1;
+    cout << ans << endl;
+    for(auto &i: a) cout << i.second + 1 << " ";
+    return 0;
+}
