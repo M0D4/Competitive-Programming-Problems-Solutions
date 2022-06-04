b5088b5edc911df016e02a8e12b63b544afc89e7
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 16:03:37 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1272A | Three Friends/66725935.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+ #define fori( tt,aa)      for(long long i= tt; i<aa; i++)
+#define forj( yy,bb)      for(long long j=yy; j<bb; j++)
+#define fork( uu,cc)      for(long long k=uu; k<cc; k++)
+#define sz(v)    v.size()
+#define all(v)   v.begin(), v.end()
+#define endl "\n";
+#define pb push_back
+#define pf push_front
+#define popb pop_back
+#define popf pop_front
+#define EPS 1e-7
+#define F first
+#define S second
+#define THINK_TWICE_CODE_ONCE; fast();
+#define to_str to_string
+ typedef long long ll;
+typedef long double ld;
+typedef double dd;
+typedef string String;
+typedef string str;
+typedef vector<int> vecint;
+typedef vector<ll> vecll;
+typedef pair<int, int> pii;
+ void fast(){
+    cin.tie(0);
+    cout.tie(0);
+    cin.sync_with_stdio(0);
+}
+ ll numOfDigits(ll n);
+ll BN (ll arr[], ll j, ll val);
+bool compare(int &a, int &b){
+    return a>b;
+}
+ int main()
+{
+    THINK_TWICE_CODE_ONCE;
+    //freopen("test cases\\Three Friends.txt", "r", stdin);
+    int t; cin>>t;
+    while(t--){
+        ll a, b,c, mn = LLONG_MAX; cin>>a>>b>>c;
+         fori(0, 3)
+            forj(0, 3)
+                fork(0, 3){
+                    ll x = a+i, y = b+j, z = c+k;
+                    mn = min(mn, abs(x-y)+abs(x-z)+abs(y-z));
+                }
+         cout<<mn<<endl;
+    }
+    return 0;
+}
+/*
+ll BN (ll arr[], ll j, ll val){
+int low = j;
+int up = n-1;
+ll ans = -1;
+while(low<=up){
+    ll mid = ((ll)low+ up)/2;
+    if(abs(arr[mid]-val)<=d){
+        ans = mid;
+        low = mid+1;
+    }else up = mid-1;
+}
+return ans;
+}*/
+ ll numOfDigits(ll n){
+    if(!n) return 1;
+    return (ll)(log10(n)+1);
+}
