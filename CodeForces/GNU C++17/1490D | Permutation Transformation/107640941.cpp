e6def744f8d2807d587e6020700bb88584cf4aa8
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:56:34 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1490D | Permutation Transformation/107640941.cpp
@@ -0,0 +1,53 @@
+#include "bits/stdc++.h"
+  #pragma GCC optimize("Ofast")
+  using namespace std;
+ #define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cerr << #x << " is " << x << endl;
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define Unique(x)       x.erase(unique(all(x)), x.end())
+#define clr(x, last)    memset(x, last, sizeof(x))
+ using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using pii = pair<int, int>;
+using tii = tuple<int, int, int>;
+ const int N = 100 + 5;
+ int a[N], ans[N], n;
+ void solve(int l, int r){
+    if(l > r)
+        return;
+    int mx = 0, pos = 0;
+    for (int i = l; i <= r; ++i) {
+        if(a[i] > mx){
+            mx = a[i];
+            pos = i;
+        }
+    }
+    ans[l]++;
+    ans[pos]--;
+    ans[pos + 1]++;
+    ans[r + 1]--;
+    solve(l, pos - 1);
+    solve(pos + 1, r);
+}
+ int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0);
+     int t; cin >> t;
+    while (t--){
+        cin >> n;
+        for (int i = 1; i <= n; ++i) {
+            cin >> a[i];
+            ans[i] = 0;
+        }
+        solve(1, n);
+        for (int i = 1; i <= n; ++i) {
+            ans[i] += ans[i - 1];
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
