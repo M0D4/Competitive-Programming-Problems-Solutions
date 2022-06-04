9d12bcb77d8b934bce9f6160a5808cd4f6e07074
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 14:19:08 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1620A | Equal or Not Equal/139829372.cpp
@@ -0,0 +1,18 @@
+#include "bits/stdc++.h"
+ #pragma GCC optimize("Ofast")
+ using namespace std;
+ #define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+ using ll = long long;
+ int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+     int tc; cin >> tc;
+    while(tc--){
+        string s; cin >> s;
+        cout << (count(all(s), 'N') == 1 ? "NO" : "YES") << endl;
+    }
+    return 0;
+}
