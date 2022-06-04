b5e0b634724e0d14fa92cb7d302363bdcab30de9
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:58:33 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1420A | Cubes Sorting/93894863.cpp
@@ -0,0 +1,28 @@
+#include "bits/stdc++.h"
+ using namespace std;
+ #define endl            "\n"
+#define sz(x)          (int) (x.size())
+#define all(v)          v.begin(), v.end()
+#define isOdd(n)       (n&1)
+#define pow(n, m)      (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+  typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+   int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+     int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        int a[n];
+        bool ok = 0;
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+            if(i && a[i] >= a[i - 1]) ok = 1;
+        }
+        cout << (ok ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
