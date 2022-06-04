45311a20149e56847aea3333e077aea93504de3b
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:56:51 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1471A | Strange Partition/103544621.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+ #pragma GCC optimize("Ofast")
+  using namespace std;
+ #define sc              scanf
+#define endl            "\n"
+#define sz(x)           (int) (x.size())
+#define all(v)          v.begin(), v.end()
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+  typedef long long ll;
+typedef double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+  int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+     int t; cin >> t;
+    while(t--){
+        int n, x; cin >> n >> x;
+        ll mn = 0, mx = 0;
+        while(n--){
+            int a; cin >> a;
+            mx += (a / x) + !!(a % x);
+            mn += a;
+        }
+        mn = (mn / x) + !!(mn % x);
+        cout << mn << " " << mx << endl;
+    }
+    return 0;
+}
