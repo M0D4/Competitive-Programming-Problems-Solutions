e79e704b96a0cb03c204e3700ee1e2d224ff181a
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 14:19:12 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1617B | GCD Problem/139564138.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+ #pragma GCC optimize("Ofast")
+ using namespace std;
+ #define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+ using ll = long long;
+ int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+     int tc; cin >> tc;
+    while(tc--){
+        ll n; cin >> n;
+        if(n & 1){
+            ll half = n / 2;
+            if(half & 1 ^ 1) cout << half - 1 << " " << half + 1 << " 1\n";
+            else cout << half - 2 << " " << half + 2 << " 1\n";
+        }else{
+            cout << "2 " << n - 3 << " 1\n";
+        }
+    }
+    return 0;
+}
