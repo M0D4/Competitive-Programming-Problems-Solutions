178db7e2312ea73c4f5aeff286c9f75eadfa0dbf
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:59:16 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1372A | Omkar and Completion/86592569.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+ using namespace std;
+ #define all(v)   v.begin(), v.end()
+#define pb       push_back
+#define sz(x)    (int) (x.size())
+#define endl "\n"
+#define isOdd(n)    (n&1)
+#define pow(n, m) (ll)powl(n, m)
+#define clr(x, val)    memset(x, val, sizeof(x)) //be careful of value and TLE
+ typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+  int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+     int t;  cin >> t;
+    while(t--){
+        int n; cin >> n;
+        while(n--){
+            cout << "1 ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
