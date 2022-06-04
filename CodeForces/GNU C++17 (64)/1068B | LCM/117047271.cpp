358d5613c860dc133ae22030f862f454c409e7cd
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:55:38 +0200

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1068B | LCM/117047271.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+ #define fori( tt,aa)      for(int i= tt; i<aa; i++)
+#define forj( yy,bb)      for(int j=yy; j<bb; j++)
+#define fork( uu,cc)      for(int k=uu; k<cc; k++)
+#define fore(v)           for(auto &it: v)
+#define all(v)   v.begin(), v.end()
+#define endl "\n";
+#define pb push_back
+#define F first
+#define S second
+#define JULEE_DO_THE_THING fast()
+#define to_str to_string
+#define isOdd(n)    (n&1)
+#define sc scanf
+#define print printf
+#define LLMAX LLONG_MAX
+#define LLMIN LLONG_MIN
+#define IMIN INT_MIN
+#define IMAX INT_MAX
+ typedef long long ll;
+typedef long double ld;
+typedef double dd;
+typedef string String;
+typedef string str;
+typedef vector<int> vecint;
+typedef vector<ll> vecll;
+typedef pair<int, int> pii;
+ void fast(){
+    cin.tie(0);
+    cout.tie(0);
+    cin.sync_with_stdio(0);
+}
+ ll numOfDigits(ll n);
+bool compare(int &a, int &b){
+    return a>b;
+}
+ ll pow (ll a, ll b);
+ dd BN (vector<pii> &v, ll n, ll m);
+  int gcd(int a, int b){
+    if(a%b==0) return b;
+    return gcd(b, a%b);
+}
+int lcm (int a, int b) { return a / gcd(a, b) * b;}
+  ll modPow(ll x, ll n, ll m) {
+    x %= m;
+    ll res = 1;
+    while (n > 0) {
+        if (n & 1)
+            res = res * x % m;
+        x = x * x % m;
+        n >>= 1;
+    }
+    return res;
+}
+ int main()
+{
+    JULEE_DO_THE_THING;
+    //freopen("test cases\\LCM.txt", "r", stdin) ;
+    ll b; cin>>b;
+    ll cnt = 0;
+    for(ll i = 1; i*i<=b; i++){
+        if(!(b%i)){
+            cnt++;
+            if(b/i!=i)cnt++;
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
+  dd BN (vector<pii> &v, ll n, ll m){
+dd low = 0;
+dd up = 1e21;
+dd ans = 0;
+while(low<=up){
+    dd mid = trunc((low+ up)/2);
+    dd fx = 0;
+    fori(0, n){
+        fx += max(0.0, mid-v[i].F)*v[i].S;
+    }
+    if(fx<=m) {
+        ans = mid;
+        low = mid+1;
+    }else up = mid-1;
+}
+return ans;
+}
+  ll pow (ll a, ll b){
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+ ll numOfDigits(ll n){
+    if(!n) return 1;
+    return (ll)(log10(n)+1);
+}
