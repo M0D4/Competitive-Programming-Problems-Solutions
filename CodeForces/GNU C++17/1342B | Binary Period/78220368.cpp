0469dc77dad51feb8757dcac3d7d1de8d0d3a746
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 16:01:19 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1342B | Binary Period/78220368.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/numeric>
+ using namespace std;
+using namespace __gnu_cxx;
+ #define all(v)   v.begin(), v.end()
+#define UNIQUE(c) (c).resize(unique(all(c)) - (c).begin())
+#define endl "\n"
+#define pb push_back
+#define F first
+#define S second
+#define START cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define isOdd(n)    (n&1)
+#define LLMAX LLONG_MAX
+#define LLMIN LLONG_MIN
+#define IMN INT_MIN
+#define IMX INT_MAX
+#define pow(n, m) power((ll)n, (ll)m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define mem(x, val)    memset(x, val, sizeof(x)) //be careful
+#define var auto
+#define MOD 1000000007
+  typedef long long ll;
+typedef long double ld;
+typedef double dd;
+typedef pair<int, int> pii;
+  int main()
+{
+    START
+    //freopen("test cases\\in.txt", "r", stdin) ;
+    int t; cin>>t;
+    while(t--){
+        string s; cin>>s;
+        int n = s.size();
+        if(count(all(s), '1')==n || count(all(s), '0')==n){
+            cout<<s<<endl;
+            continue;
+        }
+        string x = "";
+        for(int i =0; i<2*n; i+=2)
+            x +="10";
+        cout<<x<<endl;
+    }
+ return 0;
+}
