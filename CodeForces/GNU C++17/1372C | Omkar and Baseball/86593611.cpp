b6aeef7f3fed0e55d7af9816cfb3841bc0722b6c
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:59:13 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1372C | Omkar and Baseball/86593611.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+ using namespace std;
+ #define all(v)   v.begin(), v.end()
+#define pb       push_back
+#define sz(x)    (int) (x.size())
+#define endl "\n"
+#define isOdd(n)    (n&1)
+#define pow(n, m) (ll)powl(n, m)
+#define clr(x, val)    memset(x, val, sizeof(x)) //be careful of value and TLE
+ typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+  int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+     int t;  cin >> t;
+    while(t--){
+        int n; cin >> n;
+        vector<int> a(n);
+        for(auto &i: a) cin >> i;
+         int mx = 0, l = 0, r = n - 1;
+        while(l < n && a[l] == l + 1) l++;
+        while(~r && a[r] == r + 1) r--;
+        if(r < l){
+            cout << "0\n";
+            continue;
+        }
+        for(int i = l; i <= r; i++){
+            if(a[i] == i + 1){
+                mx++;
+            }
+        }
+        cout << 1 + !!mx << endl;
+    }
+    return 0;
+}
