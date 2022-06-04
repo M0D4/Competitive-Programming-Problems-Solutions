1bcd345a346257edf4eb32125b8d83e0a5432c86
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 16:00:32 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1363C | Game On Leaves/82150863.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <ext/numeric>
+/**
+       return NO_PAIN ? NO_GAIN : GAIN;
+*/
+using namespace std;
+using namespace __gnu_cxx;
+ #define all(v)   v.begin(), v.end()
+#define sz(x)    (int) x.size()
+#define Unique(x) x.erase(unique(all(x)), x.end())
+#define endl "\n"
+#define pb push_back
+#define F first
+#define S second
+#define START cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define isOdd(n)    (n&1)
+#define IMN INT_MIN
+#define IMX INT_MAX
+#define pow(n, m) power(n, m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define mem(x, val)    memset(x, val, sizeof(x)) //be careful of value and TLE
+#define MOD 1000000007
+#define EPS 1e-7
+ typedef long long ll;
+typedef long double ld;
+typedef double dd;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+  int main()
+{
+    START
+    //freopen("test cases\\in.txt", "r", stdin) ;
+    //freopen("test cases\\out.txt", "w", stdout);
+    int t; cin>>t;
+    while(t--){
+       int n, x; cin>>n>>x;
+       vector<vi> adj(n+1);
+       for(int i = 0; i+1<n; i++){
+            int u, v; cin>>u>>v;
+            adj[u].pb(v);
+            adj[v].pb(u);
+       }
+       int m = adj[x].size();
+       if(m<=1)cout<<"Ayush\n";
+       else  cout<<(!isOdd(n)?"Ayush":"Ashish")<<endl;
+    }
+ return 0;
+}
