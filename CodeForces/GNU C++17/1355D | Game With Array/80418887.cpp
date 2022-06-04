d753d1ec211a10dcffc40c104b6ebdb9712cdf9e
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 16:00:54 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1355D | Game With Array/80418887.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/numeric>
+/**
+    PROGRESS IS PROGRESS
+    NO MATTER HOW SMALL
+*/
+using namespace std;
+using namespace __gnu_cxx;
+ #define all(v)   v.begin(), v.end()
+#define sz(x)    (int) x.size()
+#define Unique(x) x.erase(unique(all(x)), x.end())
+#define endl "\n"
+#define pb push_back
+#define F first
+#define S second
+#define START cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define isOdd(n)    (n&1)
+#define LLMAX LLONG_MAX
+#define LLMIN LLONG_MIN
+#define IMN INT_MIN
+#define IMX INT_MAX
+#define pow(n, m) power((ll)n, (ll)m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define mem(x, val)    memset(x, val, sizeof(x)) //be careful of value and TLE
+#define MOD 1000000007
+ typedef long long ll;
+typedef long double ld;
+typedef double dd;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+  int main()
+{
+    START
+    //freopen("test cases\\in.txt", "r", stdin) ;
+    //freopen("test cases\\out.txt", "w", stdout) ;
+    ll n, s; cin>>n>>s;
+   ll  u = s/n, rem = s - u*(n-1);
+    if(u!=1 && rem!=1){
+        cout<<"YES\n";
+        for(int i = 0; i+1<n; i++)
+            cout<<u<<" ";
+            cout<<rem<<endl<<"1\n";
+    }
+    else cout<<"NO\n";
+  return 0;
+}
