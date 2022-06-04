4a62d362fadfbdc5a73cddfddf8586aecab186ec
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 16:04:22 +0200

    Committing files

---
+++ CodeForces/GNU C++17/977A | Wrong Subtraction/61326145.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ #define fori( tt,aa)      for(long long i= tt; i<aa; i++)
+#define forj( yy,bb)      for(long long j=yy; j<bb; j++)
+#define fork( uu,cc)      for(long long k=uu; k<cc; k++)
+#define sz(v)    v.size()
+#define all(v)   (v.begin(), v.end())
+#define endl "\n";
+#define pb push_back
+ typedef long long ll;
+typedef long double ld;
+typedef double dd;
+typedef string String;
+typedef string str;
+void fast(){
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+}
+ll pow (ll a, ll b);
+//int freq[3];
+int main()
+{
+    fast();
+    int n, k; cin>>n>>k;
+    fori(0,k){
+        if(!(n%10)) n/=10;
+        else n--;
+    }
+    cout<<n;
+    return 0;
+}
+ll pow (ll a, ll b){
+    ll pow = 1;
+    fori(0,b)
+        pow *=a;
+    return pow;
+}
