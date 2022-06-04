cee908e192898c04c1e56f3c0f4733199f1b7d4a
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-04 15:58:22 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1426B | Symmetric Matrix/94156937.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+ #pragma GCC optimize("-Ofast")
+#pragma GCC optimize("trapv")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,sse4.2,popcnt,abm,mmx,avx2,tune=native")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-funroll-all-loops,-fpeel-loops,-funswitch-loops")
+ using namespace std;
+ #define endl            "\n"
+#define sz(x)          (int) (x.size())
+#define all(v)          v.begin(), v.end()
+#define isOdd(n)       (n&1)
+#define pow(n, m)      (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+  typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+  int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+     int t; cin >> t;
+    while(t--){
+        int n, m; cin >> n >> m;
+        bool ok = 0;
+        for(int i = 0; i < n; i++){
+            int tl, tr; cin >> tl >> tr;
+            int bl, br; cin >> bl >> br;
+            if(bl == tr) ok = 1;
+        }
+        cout << (!isOdd(m) && ok ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
