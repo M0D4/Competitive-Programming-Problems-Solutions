739e76587240e9a7dafb8c04665181fd0cf884f8
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-07 23:45:04 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1514B | AND 0, Sum Big/113495175.cpp
@@ -0,0 +1,48 @@
+#include "bits/stdc++.h"
+ 
+ 
+#pragma GCC optimize("Ofast")
+ 
+ 
+using namespace std;
+ 
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define Unique(x)       x.erase(unique(all(x)), x.end())
+#define clr(x, last)    memset(x, last, sizeof(x))
+ 
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using pii = pair<ll, int>;
+using tii = tuple<int, int, int>;
+ 
+const int MOD = 1e9 + 7;
+ 
+ll modPow(ll x, ll n, ll m) {
+    x %= m;
+    ll res = 1;
+    while (n > 0) {
+        if (n & 1)
+            res = res * x % m;
+        x = x * x % m;
+        n >>= 1;
+    }
+    return res;
+}
+ 
+int main()
+{
+ 
+    ios_base::sync_with_stdio(0), cin.tie(0);
+ 
+    int tc; cin >> tc;
+    while (tc--){
+        int n, k; cin >> n >> k;
+        cout << modPow(n, k, MOD) << endl;
+    }
+    return 0;
+}
