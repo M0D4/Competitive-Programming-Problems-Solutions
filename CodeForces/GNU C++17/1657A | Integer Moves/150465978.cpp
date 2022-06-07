38643d7112308a1fc2906dfbd911f2d326cf8ab4
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-07 23:36:40 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1657A | Integer Moves/150465978.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+ 
+#pragma GCC optimize("Ofast")
+ 
+using namespace std;
+ 
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+ 
+using ll = long long;
+ 
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+ 
+    int tc; cin >> tc;
+    while(tc--){
+        int x, y; cin >> x >> y;
+        double sq = sqrt(x * x + y * y);
+        cout << 1 + (islessgreater(sq, (int)sq)) - (x == 0 && y == 0) << endl;
+    }
+    return 0;
+}
