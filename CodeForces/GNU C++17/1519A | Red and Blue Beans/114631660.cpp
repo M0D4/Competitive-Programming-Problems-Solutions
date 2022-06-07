e5f9a0890bfb736d075b9332d3982bb2dcce9107
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-07 23:44:41 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1519A | Red and Blue Beans/114631660.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+ 
+ 
+#pragma GCC optimize("Ofast")
+ 
+ 
+using namespace std;
+ 
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define clr(x, last)    memset(x, last, sizeof(x))
+ 
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using pii = pair<int, int>;
+using tii = tuple<int, int, int>;
+ 
+ 
+int main()
+{
+ 
+    ios_base::sync_with_stdio(0), cin.tie(0);
+ 
+    int t; cin >> t;
+    while(t--){
+        ll r, b, d; cin >> r >> b >> d;
+        if(r > b) swap(r, b);
+        ll cnt = b / (d + 1) + !!(b % (d + 1));
+        cout << (cnt <= r ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
