3937ff23fce082d2b9b11c26ee1cb1c225b5a9c7
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-07 23:58:26 +0200

    Committing files

---
+++ CodeForces/GNU C++17/609A | Флеш-карты/76968909.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <ext/numeric>
+ 
+using namespace std;
+using namespace __gnu_cxx;
+ 
+#define all(v)   v.begin(), v.end()
+#define endl "\n"
+#define pb push_back
+#define F first
+#define S second
+#define START cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define isOdd(n)    (n&1)
+#define LLMAX LLONG_MAX
+#define LLMIN LLONG_MIN
+#define IMN INT_MIN
+#define IMX INT_MAX
+#define pow(n, m) power((ll)n, (ll)m)
+#define numOfDigits(n)  ((n)? (ll)(log10(n))+1 : 1)
+#define mem(x, val)    memset(x, val, sizeof(x)) //be careful
+#define MOD 1000000007
+ 
+typedef long long ll;
+typedef long double ld;
+typedef double dd;
+typedef pair<int, int> pii;
+ 
+ 
+int main()
+{
+    START
+    //freopen("test cases\\.txt", "r", stdin) ;
+    int n; cin>>n;
+    int m; cin>>m;
+    int arr[n];
+    for(int &i: arr)cin>>i;
+    sort(arr, arr+n, greater<int>());
+    for(int i =0; i<n; i++){
+        m -= arr[i];
+        if(m<=0)
+            return cout<<i+1, 0;
+    }
+ return 0;
+}
+ 
