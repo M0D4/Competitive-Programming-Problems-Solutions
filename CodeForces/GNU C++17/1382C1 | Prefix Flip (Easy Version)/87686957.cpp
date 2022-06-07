4130f1c764c14fd06cb27b73236f95cda3429574
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-07 23:52:18 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1382C1 | Prefix Flip (Easy Version)/87686957.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define all(v)          v.begin(), v.end()
+#define pb              push_back
+#define sz(x)          (int) (x.size())
+#define endl            "\n"
+#define isOdd(n)       (n&1)
+#define pow(n, m)      (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x)) //be careful of value and TLE
+ 
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+ 
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        string a, b; cin >> a >> b;
+        vector<int> c;
+        if(a[0] != b[0]) c.push_back(1);
+        for(int i = 1; i < n; i++){
+            if(a[i] != b[i]){
+                c.push_back(i + 1);
+                c.push_back(1);
+                c.push_back(i + 1);
+            }
+        }
+        cout << c.size() << " ";
+        for(auto &i: c) cout << i << " ";
+    }
+    return 0;
+}
