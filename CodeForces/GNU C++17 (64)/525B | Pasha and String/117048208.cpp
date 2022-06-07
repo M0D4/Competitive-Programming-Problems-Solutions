caf4a41ef914cab2afacc4d86520c287e9a264c9
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-07 23:40:47 +0200

    Committing files

---
+++ CodeForces/GNU C++17 (64)/525B | Pasha and String/117048208.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    string s; cin>>s;
+    s = " "+s;
+    int n = s.size();
+    int pre[n+3] = {0};
+    int m; cin>>m;
+    for(int i=0; i<m; i++){
+        int a; cin>>a;
+        pre[a]++;
+        pre[n-a+1]--;
+    }
+    for(int i=1; i<n+2; i++)
+        pre[i]+=pre[i-1];
+    for(int i=1; i<=n/2; i++){
+        if(pre[i]%2!=0)
+            swap(s[i], s[n-i]);
+    }
+    cout<<s.substr(1, n-1);
+    return 0;
+}
