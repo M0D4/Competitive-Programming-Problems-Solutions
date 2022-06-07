6971932c0a1c6f71a83b7c119a945783ad4b4c79
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-07 23:47:13 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1216A | Prefixes/102490844.cpp
@@ -0,0 +1,49 @@
+#include "bits/stdc++.h"
+ 
+#pragma GCC optimize("Ofast")
+ 
+ 
+using namespace std;
+ 
+#define sc              scanf
+#define endl            "\n"
+#define sz(x)           (int) (x.size())
+#define all(v)          v.begin(), v.end()
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+ 
+ 
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+ 
+    int n; cin >> n;
+    string s; cin >> s;
+    int c = 0, a = 0, b = 0;
+    for(int i = 0; i < n; i++){
+        if(s[i] == 'a') a++;
+        else b++;
+ 
+        if(isOdd(i)){
+            if(a < b){
+                s[i] = 'a';
+                b--, a++;
+                c++;
+            }else if(b < a){
+                s[i] = 'b';
+                b++, a--;
+                c++;
+            }
+        }
+    }
+    cout << c << endl << s;
+    return 0;
+}
