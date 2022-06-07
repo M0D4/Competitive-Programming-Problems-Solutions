96546f07e40945ed43a1112cf77301eac126ea6d
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-07 23:43:16 +0200

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1285A | Mezo Playing Zoma/117047293.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+ 
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+ 
+*******************************************************************************/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n; cin>>n;
+    for(int i=0; i<n; i++){
+        char c; cin>>c;
+    }
+    cout<<n+1;
+    return 0;
+}
