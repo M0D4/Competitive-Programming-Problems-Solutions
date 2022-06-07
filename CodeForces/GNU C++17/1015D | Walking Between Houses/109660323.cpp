cd7e3aca850986403cd0ea5450506e1860d3dd37
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-07 23:45:46 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1015D | Walking Between Houses/109660323.cpp
@@ -0,0 +1,48 @@
+#include "bits/stdc++.h"
+ 
+ 
+#pragma GCC optimize("Ofast")
+ 
+ 
+using namespace std;
+ 
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define Unique(x)       x.erase(unique(all(x)), x.end())
+#define clr(x, last)    memset(x, last, sizeof(x))
+ 
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using pii = pair<ll, int>;
+using tii = tuple<int, int, int>;
+ 
+ 
+int step(int cur, int x){
+    if(cur - x > 0)
+        return cur - x;
+    return cur + x;
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    
+    ll n, k, s; cin >> n >> k >> s;
+ 
+    if (k > s || (n - 1) * k < s)
+        return cout << "NO", 0;
+ 
+    cout << "YES\n";
+    int cur = 1;
+    for (int i = k; i; --i) {
+        ll mn = min(n - 1, s - (i - 1));
+        cur = step(cur, mn);
+        s -= mn;
+        cout << cur << " ";
+    }
+    return 0;
+}
