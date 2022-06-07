32f1823a4f975ca55d74775e3c0eee5a524e48fa
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-07 23:50:42 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1426A | Floor Number/94065136.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+ 
+#define endl            "\n"
+#define sz(x)          (int) (x.size())
+#define all(v)          v.begin(), v.end()
+#define isOdd(n)       (n&1)
+#define pow(n, m)      (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+ 
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+ 
+    int t; cin >> t;
+    while(t--){
+        int n, x; cin >> n >> x;
+        if(n <= 2){
+            cout << "1\n";
+            continue;
+        }
+        int f = (n - 2) / x + ((n - 2) % x != 0);
+        cout << f + 1 << endl;
+    }
+    return 0;
+}
