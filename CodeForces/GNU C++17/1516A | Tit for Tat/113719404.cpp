37f6d4a9566779034eb3624d4da2de84ab07ab7e
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-06-07 23:44:50 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1516A | Tit for Tat/113719404.cpp
@@ -0,0 +1,48 @@
+#include "bits/stdc++.h"
+ 
+ 
+#pragma GCC optimize("Ofast")
+ 
+ 
+using namespace std;
+ 
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define isOdd(n)        (n&1)
+#define pow(n, m)       (ll)powl(n, m)
+#define Unique(x)       x.erase(unique(all(x)), x.end())
+#define clr(x, last)    memset(x, last, sizeof(x))
+ 
+ 
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using pii = pair<ll, int>;
+using tii = tuple<int, int, int>;
+ 
+void solve(){
+    int n, k; cin >> n >> k;
+    int a[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    for (int i = 0, j = n - 1; i < j && k; ++i) {
+        while (a[i] > 0 && k) --a[i], ++a[j], --k;
+    }
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << " \n"[i == n - 1];
+    }
+}
+ 
+int main()
+{
+ 
+    ios_base::sync_with_stdio(0), cin.tie(0);
+ 
+    int tc; cin >> tc;
+    while (tc--){
+        solve();
+    }
+    return 0;
+}
