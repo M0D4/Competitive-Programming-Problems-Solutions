b7986a18ca82d7321fd60d52cbef972a54bac1d9
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-07-18 01:59:17 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1703A | YES or YES?/164529475.cpp
@@ -0,0 +1,28 @@
+#include "bits/stdc++.h"
+ 
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx2")
+ 
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+ 
+using namespace std;
+using ll = long long;
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+ 
+    int TC; cin >> TC;
+    while(TC--){
+        string s; cin >> s;
+        for (int i = 0; i < 3; ++i) {
+            s[i] = tolower(s[i]);
+        }
+        cout << (s == "yes" ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
