1241241245f93840ce0ee78a56a8757d2c0f06f5
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-07-18 01:59:15 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1703B | ICPC Balloons/164529942.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+ 
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx2")
+ 
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+ 
+using namespace std;
+using ll = long long;
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+ 
+    int TC; cin >> TC;
+    while(TC--){
+        int n; cin >> n;
+        string s; cin >> s;
+        int cnt = 0;
+        int f[26] = {};
+        for (int i = 0; i < n; ++i) {
+            cnt += f[s[i] - 'A'] == 0 ? 2 : 1;
+            f[s[i] - 'A']++;
+        }
+        cout << cnt << endl;
+    }
+    return 0;
+}
