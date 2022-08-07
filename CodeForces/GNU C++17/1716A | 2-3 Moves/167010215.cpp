56f142bdc75cf26106b5408ee39b7113204f22df
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-08-07 21:59:39 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1716A | 2-3 Moves/167010215.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+ 
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx2")
+ 
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+ 
+using namespace std;
+using ll = long long;
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+ 
+    int TC; cin >> TC;
+    while(TC--){
+        ll n; cin >> n;
+        cout << (n + 2) / 3 + (n == 1) << endl;
+    }
+    return 0;
+}
