bc8af20616017bbe0bf2b2514005886e3f6ce2df
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-10-13 01:41:17 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1364B | Most socially-distanced subsequence/175281702.cpp
@@ -0,0 +1,38 @@
+#include "bits/stdc++.h"
+ 
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx2")
+ 
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+ 
+using namespace std;
+using ll = long long;
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+ 
+    int TC; cin >> TC;
+    while(TC--){
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+        vector<int> ans;
+        for (int i = 0; i < n; ++i) {
+            if (i == 0 || i + 1 == n || (a[i - 1] < a[i]) != (a[i] < a[i + 1]))
+                ans.push_back(a[i]);
+        }
+        cout << ans.size() << endl;
+        for (auto& i: ans)
+            cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
