cde51366baf984eb38cc5afdba0a45f4c4e179b5
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2022-10-23 17:57:30 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1749A | Cowardly Rooks/177229393.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+ 
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx2")
+ 
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+ 
+using namespace std;
+using ll = long long;
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+ 
+    int TC; cin >> TC;
+    while(TC--){
+        int n, m;
+        cin >> n >> m;
+ 
+        for (int i = 0; i < m; ++i) {
+            int x, y;
+            cin >> x >> y;
+        }
+        cout << (n > m ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
